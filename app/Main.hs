c56c22dc05842d768a906d6b17bd15efd0da4777
Fabian <rocaboca@gmail.com>
2016-05-02 21:56:18 +0200

    Fetches track by url and plays it

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,41 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import           Control.Lens               ((&), (.~), (^.))
-import           Control.Monad.Trans        (liftIO)
-import qualified Data.ByteString.Lazy.Char8 as BS
-import           Data.Text                  (Text, unpack)
-import qualified Network.MPD                as MPD
+import           Control.Lens             ((&), (.~), (^.), (^?))
+import           Control.Monad.Trans      (liftIO)
+import           Data.Aeson               (FromJSON)
+import           Data.Aeson.Lens          (key, _String)
+import           Data.Monoid              ((<>))
+import           Data.String              (fromString)
+import           Data.Text                (Text, pack, unpack)
+import           GHC.Generics             (Generic)
+import qualified Network.MPD              as MPD
 import           Network.Wreq
-import           System.Console.Haskeline   (InputT (), defaultSettings,
-                                             getInputLine, outputStrLn,
-                                             runInputT)
+import           System.Console.Haskeline (InputT (), defaultSettings,
+                                           getInputLine, outputStrLn, runInputT)
 
 type Repl a = InputT IO a
+
+data Track = Track
+  { id        :: Int
+  , stream_url :: Text
+  } deriving (Generic, Show, Eq)
+
+instance FromJSON Track
 
 clientID :: Text
 clientID = "f0b4138275ee2a59e4017654e9ff7c3f"
 
-getTrack :: String -> IO ()
-getTrack url = do
+fetchTrack :: Text -> IO (Maybe Track)
+fetchTrack url = do
   let opts = defaults & param "client_id" .~ [clientID]
-  res <- getWith opts url
-  BS.putStrLn $ res ^. responseBody
+                      & param "url" .~ [url]
+                      & header "Accept" .~ ["application/json"]
+
+  res <- asJSON =<< getWith opts "http://api.soundcloud.com/resolve"
+  return $ res ^? responseBody
 
 repl :: Repl ()
 repl = do
@@ -29,14 +43,19 @@ repl = do
   case trackURL of
     Nothing -> outputStrLn "Goodbye."
     Just trackURL' -> do
-      liftIO $ getTrack trackURL'
-      liftIO . MPD.withMPD $ do
-        -- MPD.add ("https://api.soundcloud.com/tracks/13158665/stream?" ++ clientID)
-        MPD.add "https://api.soundcloud.com/tracks/13158665/stream?client_id=f0b4138275ee2a59e4017654e9ff7c3f"
-        MPD.play Nothing
-        -- MPD.currentSong
-      return ()
-      -- repl
+      maybeTrack <- liftIO . fetchTrack $ pack trackURL'
+
+      case maybeTrack of
+        Nothing -> outputStrLn "The track you searched for couldn't be found"
+        Just track -> do
+          liftIO . MPD.withMPD $ do
+            liftIO $ putStrLn "Starting track..."
+            MPD.add . fromString . unpack $ stream_url track <> "?client_id=" <> clientID
+            MPD.play Nothing
+            MPD.currentSong
+
+          return ()
+      repl
 
 main :: IO ()
 main = runInputT defaultSettings repl
